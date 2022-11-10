ca50b36e496742cde45c7e1f83617f12c1b698c3
Liben Warsame <libenwarsame5@gmail.com>
2022-11-10 17:23:30 -0600

    Added controllers

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/homework-18",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/HW_18",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
