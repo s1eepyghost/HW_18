f49f79d4639c88b6df35d400db62be13d2ebe7c9
Liben Warsame <libenwarsame5@gmail.com>
2022-11-10 13:52:00 -0600

    Added config folder and server.js file

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/homework-18",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
